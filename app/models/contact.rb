65b733ee866f73497e5c991bda6395b0bea95bb1
Aaron Hill <aahill50@gmail.com>
2014-09-29 15:05:13 -0400

    done with Contact section, starting ContactShare

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,4 +2,14 @@ class Contact < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, :uniqueness => {:scope => :user_id}
   validates :user_id, presence: true
+
+
+  belongs_to(
+    :owner,
+    primary_key: :id,
+    foreign_key: :user_id,
+    class_name: "User"
+  )
+
+
 end
