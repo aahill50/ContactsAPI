3e7472e2e3b91a601b32757151e773f836b9ecee
Aaron Hill <aahill50@gmail.com>
2014-09-29 15:00:05 -0400

    migration add_index_to_email_user_id_contact_table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929184445) do
+ActiveRecord::Schema.define(version: 20140929185821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 20140929184445) do
     t.string  "email",   null: false
     t.integer "user_id", null: false
   end
+
+  add_index "contacts", ["email", "user_id"], name: "index_contacts_on_email_and_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "username",   null: false
