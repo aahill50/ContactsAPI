f2295ad18ee1c05d27df9f0480bba1f55e364e6d
Aaron Hill <aahill50@gmail.com>
2014-09-29 12:33:32 -0400

    trying to get post to work, section Nesting Parameters

--- bin/my_script.rb
+++ bin/my_script.rb
@@ -1,11 +1,43 @@
 require 'addressable/uri'
 require 'rest-client'
 
+# url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users.html'
+# ).to_s
+#
+# puts RestClient.get(url)
+
+###brain storming Now make some POST requests to /users playing around with POST data
+
+# url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/users.html'
+# ).to_s
+#
+# puts RestClient.post 'localhost:3000/users', :param1 => 'one', :nested => { :param2 => 'two' }
+#
+# #puts RestClient.post 'localhost:3000/users', { 'var1' => 'val1'}
+# #https://github.com/rest-client/rest-client
+
+
+#######
 url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/users.html'
-).to_s
+  path: '/users/5.json',
+  query_values: {
+    'some_category[a_key]' => 'another value',
+    'some_category[a_second_key]' => 'yet another value',
+    'some_category[inner_inner_hash][key]' => 'value',
+    'something_else' => 'aaahhhhh'
+  }
+)
 
-puts RestClient.get(url)
+
+puts RestClient.post(url, url.query_values)
