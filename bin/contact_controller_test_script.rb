81d1e216c6976bc1b200f963baeecc8de4b413f8
Aaron Hill <aahill50@gmail.com>
2014-09-29 16:23:25 -0400

    just finished testing update of contacts_controller

--- bin/contact_controller_test_script.rb
+++ bin/contact_controller_test_script.rb
@@ -3,13 +3,73 @@ require 'rest_client'
 
 ##### We are testing index of contacts_controller #####
 
+# url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/contacts.html'
+# ).to_s
+#
+# putsRestClient.get(url)
+
+#rake routes    <--- shows routes
+
+
+##### We are testing create of contacts_controller #####
+
+# url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/contacts.html'
+# ).to_s
+#
+# new_contact_hash = {
+#   contact: {
+#     name: "NEWEST contact",
+#     email: "emailiscool@internet.gov",
+#     user_id: 4
+#   }
+# }
+#
+# puts RestClient.post(url, new_contact_hash)
+
+#to make sure fully validated
+#rails console
+#User.find(4).contacts
+
+
+##### We are testing show of contacts_controller #####
+
+# url = Addressable::URI.new(
+#   scheme: 'http',
+#   host: 'localhost',
+#   port: 3000,
+#   path: '/contacts/3'
+# ).to_s
+#
+# puts RestClient.get(url)
+
+
+##### We are testing update of contacts_controller #####
+
 url = Addressable::URI.new(
   scheme: 'http',
   host: 'localhost',
   port: 3000,
-  path: '/contacts.html'
+  path: '/contacts/3'
 ).to_s
 
-puts RestClient.get(url)
+updated_contact_hash = {
+  contact: {
+    name: "my new name",
+    email: "ijustchange@myemail.polka"
+  }
+}
 
-#rake routes    <--- shows routes
+puts RestClient.patch(url, updated_contact_hash)
+
+
+#rails console
+#Contact.find(3)
+#should be new name instead of tom
